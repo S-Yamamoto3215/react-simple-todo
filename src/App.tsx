6dccc11dc0bb38703f509ebd6676746615c6987d
S-Yamamoto3215 <yamamotoshoya8@gmail.com>
2023-01-18 16:30:29 +0900

    Simple MarkUp

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,44 @@
 import React from 'react';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      ここにアプリを作る
+      <h1>ToDo List</h1>
+      {/* TodoInputField */}
+      <div className='TodoInputField'>
+        <input type="text" placeholder='e.x) 〇〇をする'/>
+        <button>→</button>
+      </div>
+
+      {/* TodoList */}
+      <div className='TodoList'>
+
+        {/* FilteringMenu */}
+        <div className='FilteringMenu'>
+          <button>All</button>
+          <button>Active</button>
+          <button>Completed</button>
+        </div>
+
+        {/* TodoItem */}
+        <div className='TodoItem'>
+          <input type="checkbox" name="todoItem" id="aaa" />
+          ここにTodoの内容が入ります
+        </div>
+        <div className='TodoItem'>
+          <input type="checkbox" name="todoItem" id="aaa" />
+          ここにTodoの内容が入ります
+        </div>
+        <div className='TodoItem'>
+          <input type="checkbox" name="todoItem" id="aaa" />
+          ここにTodoの内容が入ります
+        </div>
+
+        {/* CompletedClearBtton */}
+        <div className='CompletedClearBtton'>
+          <button>CompletedClearBtton</button>
+        </div>
+      </div>
     </div>
   );
 }
