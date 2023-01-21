b0a400e2be66cd70e0640d3719882293f4af4641
S-Yamamoto3215 <yamamotoshoya8@gmail.com>
2023-01-21 23:18:32 +0900

    TodoList Component create file

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,7 @@ import React from 'react';
 
 import { ChakraProvider } from '@chakra-ui/react'
 
-import { TodoItem } from "./components/TodoItem";
-import { FilteringMenu } from "./components/FilteringMenu";
-import { CompletedClearBtton } from "./components/CompletedClearBtton";
+import { TodoList } from "./components/TodoList";
 import { TodoInputField } from "./components/TodoInputField";
 
 const App = () => {
@@ -19,39 +17,12 @@ const App = () => {
 
 export default App;
 
-type TodoType = {
-  id: string;
-  content: string;
-  isCompleted: boolean;
-}
-
-type TodoListType = TodoType[];
-
 const TodoListBase = () => {
   return (
     <div className='TodoListBase'>
       <h1>ToDo List</h1>
       <TodoInputField />
       <TodoList />
-    </div>
-  );
-}
-
-const TodoList = () => {
-  const todoData: TodoListType = [
-    {id: "1", content: 'タスクA', isCompleted: false},
-    {id: "2", content: 'タスクB', isCompleted: false},
-    {id: "3", content: 'タスクC', isCompleted: true},
-    {id: "4", content: 'タスクD', isCompleted: false},
-  ];
-
-  return (
-    <div className='TodoList'>
-      <FilteringMenu />
-      <ul className='TodoItemList'>
-        {todoData.map((todo) => <TodoItem key={todo.id} id={todo.id} content={todo.content} isCompleted={todo.isCompleted} />)}
-      </ul>
-      <CompletedClearBtton />
     </div>
   );
 }
