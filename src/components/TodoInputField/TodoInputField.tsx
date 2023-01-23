16a1fe9ed48a7fd7d83c241c1c6ec7cf71aa4e51
S-Yamamoto3215 <yamamotoshoya8@gmail.com>
2023-01-23 21:07:42 +0900

    inputFiledをState化

--- src/components/TodoInputField/TodoInputField.tsx
+++ src/components/TodoInputField/TodoInputField.tsx
@@ -1,10 +1,21 @@
+import { useState } from 'react';
 import { AddIcon } from '@chakra-ui/icons';
 import { Button, Input, InputGroup, InputRightElement } from '@chakra-ui/react'
 
 export const TodoInputField = () => {
+  const [inputValue, setInputValue] = useState<string>('')
+
+  const inputOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value)
+  }
+
   return (
     <InputGroup size="md">
-      <Input placeholder='e.x) 〇〇をする' />
+      <Input
+        placeholder='e.x) 〇〇をする'
+        value={inputValue}
+        onChange={inputOnChange}
+      />
       <InputRightElement>
         <Button>
           <AddIcon />
