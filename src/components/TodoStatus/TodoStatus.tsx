674ea6e1158588e1e10a5ca1f1cc11d2d6347029
S-Yamamoto3215 <yamamotoshoya8@gmail.com>
2023-02-03 21:25:35 +0900

    :sparkles: タスクの数を表示

--- src/components/TodoStatus/TodoStatus.tsx
+++ src/components/TodoStatus/TodoStatus.tsx
@@ -1,13 +1,31 @@
 import { useContext } from 'react';
-import { Progress } from '@chakra-ui/react'
+import { Flex, Progress, Text } from '@chakra-ui/react'
 
 import { TodoContext } from "../../providers/TodoProvider";
 
 
 export const TodoStatus = () => {
-  const { progressRateCalc } = useContext(TodoContext);
+  const { todos } = useContext(TodoContext);
+
+  const progressRateCalc = () => {
+    const completedTodos = todos.filter(todo => todo.isCompleted);
+    if (completedTodos.length === 0) {
+      return 0;
+    }
+    const progressRate = completedTodos.length / todos.length * 100;
+    return progressRate;
+  }
 
   return (
-    <Progress value={progressRateCalc()} isAnimated hasStripe />
+    <>
+      <Flex mb={2}>
+        <Text fontSize='sm' fontWeight='bold'>すべてのタスク：{todos.length}</Text>
+        <Text fontSize='sm' fontWeight='bold' mx={4}>/</Text>
+        <Text fontSize='sm' fontWeight='bold' color='red.500'>未完了：{todos.filter(todo => !todo.isCompleted).length}</Text>
+        <Text fontSize='sm' fontWeight='bold' mx={4}>/</Text>
+        <Text fontSize='sm' fontWeight='bold' color='teal.500'>完了：{todos.filter(todo => todo.isCompleted).length}</Text>
+      </Flex>
+      <Progress value={progressRateCalc()} isAnimated hasStripe />
+    </>
   );
 }
