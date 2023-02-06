a6f26c54bf00df2228165d65031af9f0765686a7
S-Yamamoto3215 <yamamotoshoya8@gmail.com>
2023-02-06 21:16:44 +0900

    :sparkles: #18 dnd-kit Sortableの仮実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { ChakraProvider, Container } from '@chakra-ui/react'
 
 import { TodoListBase } from './components/TodoListBase'
+import { SortableBase } from './components/SortableBase'
 
 const App = () => {
   return (
     <ChakraProvider>
       <Container maxW='2xl' centerContent>
         <TodoListBase />
+        <SortableBase />
       </Container>
     </ChakraProvider>
   );
---
+++ src/components/SortableBase/index.ts
@@ -0,0 +1,1 @@
+export { SortableBase } from './SortableBase'
